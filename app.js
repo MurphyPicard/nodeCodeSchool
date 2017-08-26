23f07b2f39da8bfa2b3480e3b835bb3b53d5f736
Ara <ayaghsizian@gmail.com>
2017-08-26 12:20:22 -0400

    tested file connections

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ gb.goodbye();
 myMod.foo(5, 3);
 myMod.bar(5, 3);
 
-var makeRequest = require('./make_request');
+// var makeRequest = require('C:/Users/ADMIN/Desktop/nonProjects/nodeCodeSchool/make_request');
+var makeRequest = require('./make_request'); // easier
 makeRequest("Hello Ed");
 makeRequest('Hello Will');
