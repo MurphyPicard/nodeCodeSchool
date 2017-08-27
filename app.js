e5e1f9c0ffdcb8c869e23612bc1bac03bc4d52a2
Ara <ayaghsizian@gmail.com>
2017-08-27 03:41:42 -0400

    using express to get tweets from twitter

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-var hello = require('./custom_hello');
-var gb = require('./custom_goodbye');
-var myMod = require('./my_module');
+// var hello = require('./custom_hello');
+// var gb = require('./custom_goodbye');
+// var myMod = require('./my_module');
+//
+// hello();
+// gb.goodbye();
+// myMod.foo(5, 3);
+// myMod.bar(5, 3);
+//
+// // var makeRequest = require('C:/Users/ADMIN/Desktop/nonProjects/nodeCodeSchool/make_request');
+// var makeRequest = require('./make_request'); // easier
+// //npm search request
+// makeRequest("Hello Ed");
+// makeRequest('Hello Will');
+// makeRequest('hi');
 
-hello();
-gb.goodbye();
-myMod.foo(5, 3);
-myMod.bar(5, 3);
+var express = require('express');
+var app = express();
 
-// var makeRequest = require('C:/Users/ADMIN/Desktop/nonProjects/nodeCodeSchool/make_request');
-var makeRequest = require('./make_request'); // easier
-//npm search request
-makeRequest("Hello Ed");
-makeRequest('Hello Will');
-makeRequest('hi');
+var request = require('request')
+var url = require('url')
+
+app.get('/tweets/:username', (req, res)=>{
+  var username = req.params.username;
+  options = {
+    protocol: "http:",
+    host: 'api.twitter.com',
+    pathname: '/1/statuses/user_timeline.json',
+    query: { screen_name: username, count: 10}
+  }
+  var twitterUrl = url.format(options);
+  request(twitterUrl).pipe(res);
+});
